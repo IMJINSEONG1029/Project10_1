fb0c64392fc0b24d2866f9415e93db80f1e42cda
IMJINSEONG1029 <131164287+IMJINSEONG1029@users.noreply.github.com>
2023-06-12 16:36:34 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/project10_1/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.cookandroid.project10_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private Button btnReturn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+
+        init();
+        initLr();
+    }
+
+    public void init(){
+        btnReturn = findViewById(R.id.btnReturn);
+    }
+
+    public void initLr(){
+        btnReturn.setOnClickListener(v -> {
+            finish();
+        });
+    }
+}
